126dbbadcd500f038d62e3dc04022672a5da9350
Hannah Eves <heves.work@outlook.com>
2024-06-21 12:09:05 +0000

    Set up structure of game functions

---
+++ assets/script.js
@@ -0,0 +1,39 @@
+//runGame()
+function runGame() {
+
+}
+
+//checkAnswer()
+function checkAnswer() {
+
+}
+
+//calculateCorrectAnswer()
+function calculateCorrectAnswer () {
+
+}
+
+//incrementScore()
+function incrementScore() {
+
+}
+
+//incrementWrongAnswer()
+function incrementWrongAnswer() {
+
+}
+
+//displayAdditionQuestion()
+function displayAdditionQuestion() {
+
+}
+
+//displaySubtractQuestion()
+function displaySubtractQuestion() {
+
+}
+
+//displayMultiplyQuestion()
+function displayMultiplyQuestion() {
+    
+}
