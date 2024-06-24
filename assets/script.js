8e5ddc8dcc9c17e9609a7db732be2d9e418fad19
Hannah Eves <heves.work@outlook.com>
2024-06-24 09:34:52 +0000

    Add multiplication game code

--- assets/script.js
+++ assets/script.js
@@ -30,6 +30,12 @@ function runGame(gameType) {
 
     if (gameType === "addition") {
         displayAdditionQuestion(num1, num2);
+    } else if (gameType === "multiply") {
+        displayMultiplyQuestion(num1, num2);
+    } else if (gameType === "subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else if (gameType === "division") {
+        displayDivisionQuestion(num1, num2);
     } else {
         alert(`Unknown game type: ${gameType}`);
         throw `Unknown game type: ${gameType}. Aborting!`;
@@ -73,7 +79,9 @@ function calculateCorrectAnswer() {
 
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
-    } else {
+    } else if (operator === "x") {
+        return [operand1 * operand2, "multiply"]; }
+    else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -113,12 +121,22 @@ function displayAdditionQuestion(operand1, operand2) {
 
 }
 
-//displaySubtractQuestion()
-function displaySubtractQuestion() {
+//displayMultiplyQuestion()
+function displayMultiplyQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "x";
 
 }
 
-//displayMultiplyQuestion()
-function displayMultiplyQuestion() {
+
+//displaySubtractQuestion()
+function displaySubtractQuestion(operand1, operand2) {
+
+}
+
+//displayDivisionQuestion()
+function displayDivisionQuestion(operand1, operand2) {
 
 }
