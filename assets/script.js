5939ec8db8ab94c890ed9f0bc74570482cddee56
Hannah Eves <heves.work@outlook.com>
2024-06-24 09:41:46 +0000

    Add code for subraction game

--- assets/script.js
+++ assets/script.js
@@ -80,8 +80,12 @@ function calculateCorrectAnswer() {
     if (operator === "+") {
         return [operand1 + operand2, "addition"];
     } else if (operator === "x") {
-        return [operand1 * operand2, "multiply"]; }
-    else {
+        return [operand1 * operand2, "multiply"];
+    } else if (operator === "-") {
+        return [operand1 - operand2, "subtract"];
+    } else if (operator === "/") {
+        return [operand1 / operand2, "division"];
+    } else {
         alert(`Unimplemented operator ${operator}`);
         throw `Unimplemented operator ${operator}. Aborting!`;
     }
@@ -133,6 +137,10 @@ function displayMultiplyQuestion(operand1, operand2) {
 
 //displaySubtractQuestion()
 function displaySubtractQuestion(operand1, operand2) {
+
+    document.getElementById('operand1').textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById('operand2').textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById('operator').textContent = "-";
 
 }
 
